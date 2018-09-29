831b16e2100825252c8e366628fb84b1e2bd0a67
pawlactb <pawlactb@clarkson.edu>
2018-09-29 19:04:22 -0400

    Refactored "DataStoring" to "DataCustomer". Removed PassThroughPipeline, and gave it's functionality to the default pipeline

--- DataHandling/Pipeline.py
+++ DataHandling/Pipeline.py
@@ -1,32 +1,31 @@
-from abc import ABC, abstractmethod
-
 import pandas as pd
 
 
-class Pipeline(ABC):
+class Pipeline():
 
-    def __init__(self, input=None, output=None):
-        self.input = input
-        self.output = output
+    def __init__(self):
+        self.producers = {}
+        self.consumers = {}
         self.data = pd.DataFrame()
         pass
 
     def process(self):
         pass
 
-    @abstractmethod
+    def attach_consumer(self, name, consumer):
+        self.consumers[name] = consumer
+
+    def attach_producer(self, name, consumer):
+        self.producers[name] = consumer
+
+    def unattach_consumer(self, name):
+        self.consumers.pop(name)
+
+    def unattach_producer(self, name):
+        self.consumers.pop(name)
+
     def accept_data(self, data):
-        pass
-
-    @abstractmethod
-    def publish_data(self):
-        pass
-
-
-class PassThroughPipeline(Pipeline):
-
-    def accept_data(self, data):
-        self.data = data;
+        self.data = data
 
     def publish_data(self):
-        self.output.accept_data(self.data)
+        map(lambda x: x.accept_data(self.data), [consumer for consumer in self.consumers.values()])
