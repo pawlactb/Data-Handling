581663345dd1b81fc3527e0f5ad4f5bcd5d83c66
pawlactb <pawlactb@clarkson.edu>
2018-09-30 16:21:20 -0400

    Added documentation.

--- Nodes.py
+++ Nodes.py
@@ -4,6 +4,12 @@ import pandas as pd
 class Node(object):
 
     def __init__(self, name="Unnamed Node", upstream=None):
+        """
+        Initialize the Node.
+        :param str name: Node Name (should be unique).
+        :param list upstream: Upstream nodes.
+        """
+
         self.data = pd.DataFrame()
         self.name = name
         self.upstream = dict()
@@ -14,21 +20,49 @@ class Node(object):
                 self.attach_upstream([up])
 
     def __repr__(self):
+        """
+        Provide print representation of the Pipeline.
+        :return: str representation of the Node.
+        """
+
         upstream = self.upstream.keys()
         downstream = self.downstream.keys()
         return "Node: " + self.name + " with inputs: " + " ,".join(upstream) + ", and outputs: " + " ,".join(downstream)
 
     def attach_upstream(self, upstream):
+        """
+        Attach one or more upstream nodes.
+        :param list upstream: list of upstream Nodes.
+        :return: None
+        """
+
         for up in upstream:
             self.upstream[up.name] = up
             up.attach_downstream([self])
 
     def attach_downstream(self, downstream):
+        """
+        Attach downstream node(s).
+        :param list downstream: downstream nodes.
+        :return: None
+        """
+
         for down in downstream:
             self.downstream[down.name] = down
 
     def receive_data(self, data):
+        """
+        Receive data from upstream.
+        :param Pandas.Dataframe data: The data to receive.
+        :return: None
+        """
+
         self.data = pd.DataFrame.copy(data)
 
     def process_data(self):
+        """
+        Process contained data.
+        :return: None
+        """
+
         pass
--- Pipelines.py
+++ Pipelines.py
@@ -3,19 +3,26 @@ from Nodes import Node
 
 class Pipeline(Node):
 
-    def __init__(self, name="Unnamed Pipeline", inputs=None, outputs=None):
-        super().__init__(name, inputs)
+    def __init__(self, name="Unnamed Pipeline", upstream=None, downstream=None):
+        """
+        Initialize the Pipeline.
+        :param name: Pipeline Name (should be unique).
+        :param upstream: Upstream node(s).
+        :param downstream: Downstream node(s).
+        """
+        super().__init__(name, upstream)
 
         self.outputs = dict()
-        if outputs is not None:
-            for output in outputs:
+        if downstream is not None:
+            for output in downstream:
                 self.outputs.update([(output.name, output)])
                 output.attach_upstream([self])
 
     def __repr__(self):
+        """
+        Provide print representation of the Pipeline.
+        :return: String representation of the Pipeline.
+        """
         upstream = self.upstream.keys()
         downstream = self.outputs.keys()
         return "Pipeline: " + self.name + " connecting: " + " ,".join(upstream) + " to: " + ", ".join(downstream)
-
-    def process_data(self):
-        print(self.data.head(5))
